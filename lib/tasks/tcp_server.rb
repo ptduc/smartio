8c269ff8ce762f622d54f1dfe3a54c1f67fc13ad
PhanThienDucGA <p_h_duc@ga-tech.co.jp>
2018-10-05 18:15:59 +0700

    add gem connect

--- lib/tasks/tcp_server.rb
+++ lib/tasks/tcp_server.rb
@@ -1,11 +1,15 @@
 require 'socket'
 require 'json'
 require 'pg'
-
+require 'byebug'
 server = TCPServer.new 3003
 
 abc = {}
 n = 1
+DB_INFO='postgres://smartio:smartio@kynguyenxanh.net:5432/smartio_development'
+conn = PG::Connection.new(DB_INFO)
+res = conn.exec_params("INSERT INTO devices(\"code\", \"unit_id\", \"is_active\", \"created_at\", \"updated_at\") VALUES ('00001', 1, '1','#{Time.now}','#{Time.now}')")
+puts res
 loop do
   Thread.start(server.accept) do |client|
     abc[n] = client
@@ -20,25 +24,4 @@ loop do
     end
     n = n + 1
   end
-end
-
-def 
-  begin
-    adapter: postgresql
-    encoding: unicode
-    host: kynguyenxanh.net
-    username: smartio
-    password: smartio
-    con = PG.connect(host: host, dbname: dbname, user: user, password: passwd, port: 5432)
-
-    user = con.user
-    db_name = con.db
-    pswd = con.pass
-    
-    puts "User: #{user}"
-    puts "Database name: #{db_name}"
-    puts "Password: #{pswd}" 
-    
-rescue PG::Error => e
-    puts e.message  
 end
