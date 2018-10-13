be190ac459ec4fe02bb4e5b3b58bb178045957c1
DucPT <p_h_duc@ga-tech.co.jp>
2018-10-13 22:29:05 +0700

    delete command and model

--- app/models/status_history.rb
+++ app/models/status_history.rb
@@ -1,11 +1,11 @@
 class StatusHistory < ApplicationRecord
   belongs_to :device
-  enum relay1_mode: { manual: 0, auto: 1 }
-  enum replay1_status: { off: 0, on: 1 }
-  enum relay2_mode: { manual: 0, auto: 1 }
-  enum replay2_status: { off: 0, on: 1 }
-  enum relay3_mode: { manual: 0, auto: 1 }
-  enum replay3_status: { off: 0, on: 1 }
-  enum relay4_mode: { manual: 0, auto: 1 }
-  enum replay4_status: { off: 0, on: 1 }
+  # enum relay1_mode: { manual: 0, auto: 1 }
+  # enum replay1_status: { off: 0, on: 1 }
+  # enum relay2_mode: { manual: 0, auto: 1 }
+  # enum replay2_status: { off: 0, on: 1 }
+  # enum relay3_mode: { manual: 0, auto: 1 }
+  # enum replay3_status: { off: 0, on: 1 }
+  # enum relay4_mode: { manual: 0, auto: 1 }
+  # enum replay4_status: { off: 0, on: 1 }
 end
